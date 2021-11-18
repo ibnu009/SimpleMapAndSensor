27cf00359648db37b41a3d63093a725f89e91bb0
ibnu009 <ibnubatutah001@gmail.com>
2021-11-18 17:43:10 +0700

    Initial commit

---
+++ app/src/main/java/com/ibnu/sensor/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.ibnu.sensor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val buttonMap = findViewById<Button>(R.id.btnMap)
+        buttonMap.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonSensor = findViewById<Button>(R.id.btnSensor)
+        buttonSensor.setOnClickListener {
+            val intent = Intent(this, AccelerometerActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
